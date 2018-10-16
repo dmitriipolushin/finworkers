1034f99d7f4da17fc336ee6be93d06069a9dbf2a
DmitriiPolushin <27102001@bk.ru>
2018-10-16 21:39:16 +0300

    costs added

--- player.py
+++ player.py
@@ -5,6 +5,5 @@ loan1 = {'qua': '10000', 'dL': '15'}
 playerLoans = [loan1]
 buildings = [bCommonFactory, bAutoFactory, bUpgrade]
 dollars = 10000
-player = {'money': dollars, 'factory': '1', 'autoFactory': '1', 'esm': '5',
-         'egp': '5', 'buildings': buildings, 'loans': playerLoans}
-print(player['buildings'][0])
+player = {'money': dollars, 'factory': 1, 'autoFactory': 1, 'esm': 5,
+         'egp': 5, 'buildings': buildings, 'loans': playerLoans}
