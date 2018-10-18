a6f47c2b85772bf014933a96d92ffd6a09056e3a
DmitriiPolushin <27102001@bk.ru>
2018-10-18 22:22:08 +0300

    something upgrade

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import player
 import costs
 print('Добро пожаловать в игру финансовые воротилы!')
-print('\n Сколько будет игрроков?')
-curNumOfGamers = int(input())
-players = []
-for i in range(curNumOfGamers):
-    players.append(player.player)
+player
 for i in range(len(players)):
     costs.monthlyCosts(players[i])
 print(players[0]['money']) 
--- player.py
+++ player.py
@@ -1,28 +1,32 @@
-building = {'view': 0,'DL': 0} # Создаем единую для всех видов строительства запиську билдинг
-loan = {'qnt', 'DL'}# Это записька ссуды: qnt - сумма ссуды, DL - дата выплаты(Дедлайн)
-buildings = [] * 6
-for i in range(6):
-    buildings.append(building) # В итоге имеем массив из 6 записек билдингов(макс. кол-во фабрик, которое можно построить)
-playerLoans = []
-for i in range (6):
-    playerLoans.append(loan) # ссуды берутся под фабрики, поэтому их тоже 6
+def players():
+    building = {'view': 0,'DL': 0} # Создаем единую для всех видов строительства запиську билдинг
+    loan = {'qnt', 'DL'}# Это записька ссуды: qnt - сумма ссуды, DL - дата выплаты(Дедлайн)
+    buildings = [] * 6
+    for i in range(6):
+        buildings.append(building) # В итоге имеем массив из 6 записек билдингов(макс. кол-во фабрик, которое можно построить)
+    playerLoans = []
+    for i in range (6):
+        playerLoans.append(loan) # ссуды берутся под фабрики, поэтому их тоже 6
 """
  В принципе сейчас мы можем так и не делать, а добавлять новую ссуду или стройку в массив по ходу его появления
 """
 
 # Непосредственно записька игрока
 
-player = {'money': 10000,      # Денежки
-          'factory': 2,        # Кол-во обыч. Фабрик
-          'autoFactory': 0,    # Кол-во автомат.Фабрик
-          'esm': 4,            # Кол-во ЕСМ
-          'egp': 2,            # Кол-во ЕГП
-          'buildings': buildings,# Все происходящие в данный момент строительства
-          'loans': playerLoans,  # Массив с ссудами игрока
-         }
-players = []
-print('\n Сколько будет игрроков?')
-curNumOfGamers = int(input())
-for i in range(curNumOfGamers):
-    players.append(player)
+    player = {'money': 10000,      # Денежки
+        'factory': 2,        # Кол-во обыч. Фабрик
+        'autoFactory': 0,    # Кол-во автомат.Фабрик
+        'esm': 4,            # Кол-во ЕСМ
+        'egp': 2,            # Кол-во ЕГП
+        'buildings': buildings,# Все происходящие в данный момент строительства
+        'loans': playerLoans,  # Массив с ссудами игрока
+        }
+    global players
+    players = []
+    print('\n Сколько будет игрроков?')
+    global curNumOfGamers
+    curNumOfGamers = int(input())
+    for i in range(curNumOfGamers):
+        players.append(player)
+    return players, curNumOfGamers
 
